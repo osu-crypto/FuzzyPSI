8d82a3c2e4671cdbd74a51857890f42d96dfa51e
Peter Rindal <peterrindal@gmail.com>
2020-11-15 14:22:24 -0800

    coproto integration

---
+++ libOTe/Tools/CoprotoSock.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "coproto/Scheduler.h"
+#include "cryptoTools/Network/Channel.h"
+
+namespace osuCrypto
+{
+
+	struct CoprotoSock : public coproto::Socket
+	{
+		Channel mChl;
+		CoprotoSock(Channel& chl)
+			: mChl(chl)
+		{}
+
+
+		coproto::error_code recv(coproto::span<u8> data) override
+		{
+			try {
+				mChl.recv(data);
+			}
+			catch (BadReceiveBufferSize& b)
+			{
+				return coproto::code::badBufferSize;
+			}
+
+			return {};
+		}
+		coproto::error_code send(coproto::span<u8> data)override
+		{
+			mChl.send(data);
+			return {};
+		};
+		void cancel()override
+		{
+			mChl.cancel();
+		}
+	};
+}
