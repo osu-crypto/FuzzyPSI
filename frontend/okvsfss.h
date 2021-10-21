36e610003894841d7a8a0163f437e62e74ff6b6e
gayathrigarimella <46950313+gayathrigarimella@users.noreply.github.com>
2021-10-21 15:03:45 -0700

    few more comments

--- frontend/okvsfss.h
+++ frontend/okvsfss.h
@@ -25,5 +25,15 @@
 void PaxosEncode(const std::vector<uint64_t> setKeys, const std::vector<osuCrypto::block> setValues0, const std::vector<osuCrypto::block> setValues1, std::vector<osuCrypto::block>& okvs0, std::vector<osuCrypto::block>& okvs1, uint64_t fieldSize);
 void far_apart_FssShare(uint64_t delta, int nSquares, std::vector<osuCrypto::block>& okvs0, std::vector<osuCrypto::block>& okvs1);
 void far_apart_FssEval(uint64_t x_coord, uint64_t y_coord, vector<osuCrypto::block> okvs, uint64_t delta, uint64_t hashSize);
-void batchFssEval(vector<uint64_t> x_coord, vector<uint64_t> y_coord, vector<osuCrypto::block> okvs, uint64_t delta, uint64_t hashSize);
+
+// Functions tailored for the PSI protocol 
+
+// 1. Eval function by the PSI sender who computes around 1M points
+// # TODO : batch evaluation of points
+void psi_FssEval(uint64_t x_coord, uint64_t y_coord, vector<vector<osuCrypto::block>> okvs, uint64_t delta, uint64_t hashSize);
+
+//Eval function by the PSI receiver who computes Eval over entire domain
+void psiRecver_FssEval(uint64_t nSquares, vector<vector<osuCrypto::block>> okvs, uint64_t delta, uint64_t nkeys);
+
+//void batchFssEval(vector<uint64_t> x_coord, vector<uint64_t> y_coord, vector<osuCrypto::block> okvs, uint64_t delta, uint64_t hashSize);
 //void FssShare_Enumerate(uint64_t delta, int nSquares, vector<vector<osuCrypto::block>> &okvs0, vector<vector<osuCrypto::block>> &okvs1);
